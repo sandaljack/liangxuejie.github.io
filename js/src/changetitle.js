787dd679257f64ea051d683184d6a693bcfbbf69
sandaljack <425862717@qq.com>
2017-08-22 23:04:50 +0800

    Site updated: 2017-08-22 23:04:42

--- js/src/changetitle.js
+++ js/src/changetitle.js
@@ -1,5 +1,9 @@
 
-var id = Math.ceil(Math.random()*5);
+// max - 期望的最大值 
+var max = 13;
+// var id = parseInt(Math.random()*(max+1),10); 
+var id = Math.floor(Math.random()*(max+1)); 
+
 var content = [
 	'大爷听过我的歌，小伙亲过我的脸',
 	'穷则独善其身，富则妻妾成群',
@@ -7,15 +11,19 @@ var content = [
 	'如果天黑之前来得及，我要挖了你的眼睛',
 	'千万别说你一无所有，至少你还有病啊',
 	'相爱没有那么容易，每个人都有他的手机',
-	'答应我你从此不在深夜里排队',
 	'想飞上天和太阳肩并肩',
 	'书到用时方恨少，钱到月底不够花',
-	'我们坐在高高的骨灰旁边～～',
 	'两个黄鹂鸣翠柳，一行白鹭上西天',
 	'你所有为人称道的美丽，都有PS的痕迹',
 	'天生我材必有用，老鼠儿子会打洞',
-	'你伤害了word，还',
+	'你伤害了word，还excel而过',
+	'答应我你从此不在深夜里排队',
+	'我们坐在高高的骨灰旁边',
 ];
+
 $('.site-title').html(content[id]);
-// $('.logo-line-before').css('color:#383838');
-// $('.logo-line-after').css('color:#383838');
+$('.logo-line-before').attr('style', 'width:90%');
+$('.logo-line-after').attr('style', 'width:90%');
+// $('.logo-line-after').parents().parents().parents().after('<br>');
+// $('.logo-line-after').parent('custom-logo-site-title').siblings('site-nav').before('<br>');
+$('.site-nav').before('<br><br>').children('.menu').attr('style','float:left');
