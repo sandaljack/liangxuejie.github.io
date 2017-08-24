61ae71995a9196cd27cd574e67f2d5275f153318
sandaljack <425862717@qq.com>
2017-08-24 16:01:20 +0800

    Site updated: 2017-08-24 16:01:09

--- js/src/changetitle.js
+++ js/src/changetitle.js
@@ -1,6 +1,6 @@
 
 // max - 期望的最大值 
-var max = 13;
+var max = 14;
 // var id = parseInt(Math.random()*(max+1),10); 
 var id = Math.floor(Math.random()*(max+1)); 
 
@@ -13,6 +13,7 @@ var content = [
 	'相爱没有那么容易，每个人都有他的手机',
 	'想飞上天和太阳肩并肩',
 	'书到用时方恨少，钱到月底不够花',
+	'问君能有几多愁，恰似一群太监上青楼',
 	'两个黄鹂鸣翠柳，一行白鹭上西天',
 	'你所有为人称道的美丽，都有PS的痕迹',
 	'天生我材必有用，老鼠儿子会打洞',
@@ -27,3 +28,6 @@ $('.logo-line-after').attr('style', 'width:90%');
 // $('.logo-line-after').parents().parents().parents().after('<br>');
 // $('.logo-line-after').parent('custom-logo-site-title').siblings('site-nav').before('<br>');
 $('.site-nav').before('<br><br>').children('.menu').attr('style','float:left');
+
+$('#menu').append('<li class="menu-item"><a href="/liangxuejie.github.io/snake.html" rel="section"><i class="menu-item-icon fa fa-fw fa-sitemap"></i>娱乐</a></li>');
+
